b9a017fd879065953489510343c35e187072a0ae
muhsufyan <muh.sufyan.ts@gmail.com>
2022-08-23 17:39:16 +0700

    path param done

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # fastapi-doc
+## how to run
 python -m venv virtualenv<br>
 windows => .\virtualenv\Scripts\activate.bat<br>
 pip install "fastapi[all]" <br>
 update => virtualenv\Scripts\python.exe -m pip install --upgrade pip<br>
 run => uvicorn main:app --reload<br>
 * dimana main adlah file main.py & app adlah FastAPI()<br>
+
+# path param
+ex the param is foo => http://127.0.0.1:8000/items/foo<br>
+path param select data from list => /models/{model_name}<br>
+path param for file => /files/{file_path}<br>
--- main.py
+++ main.py
@@ -7,3 +7,41 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+# catch path param with argument func (item_id param read_item func will catch item_id data url) but the data from url is not define data type
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/search/item/{item_id}")
+# catch path param with argument func (item_id param read_item func will catch item_id data url).
+# and the data from url is must int
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+# pilih salah satu data dari list data (like gender, tingkatan/level). contoh model ml yg bisa dipilih
+from enum import Enum
+# list of model ML
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+"""
+path param untuk file
+ex path file home/johndoe/myfile.txt & url is /files/{file_path}
+so url will be /files/home/johndoe/myfile.txt
+"""
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
