4a6fdae9ca62255209d50d1c1cc1380da45158d1
muhsufyan <muh.sufyan.ts@gmail.com>
2022-08-31 13:19:42 +0700

    done

--- README.md
+++ README.md
@@ -12,12 +12,7 @@ di python 3.6, OR pd param func menggunakan union<br>
 => q: Union[str, None] = None<br>
 sedangkan untuk 3.10<br>
 =>  q: str | None = None
-# FORM DATA
-data input selain berupa json dpt pula form input, gunakan Form<br>
-install dulu => pip install python-multipart.<br>
-With <b>Form</b> you can declare the same metadata and validation as with <b>Body</b> (and <b>Query, Path, Cookie</b>). karena <b>Form</b> is a class that inherits directly from <b>Body</b>.<br>
-Data from forms is normally encoded using the "media type" application/x-www-form-urlencoded.
-But when the form includes files, it is encoded as multipart/form-data<br>
-WARNING: You can declare multiple Form parameters in a path operation, but you can't also declare Body fields that you expect to receive as JSON, as the request will have the body encoded using application/x-www-form-urlencoded instead of application/json.
-
-<BR>This is not a limitation of FastAPI, it's part of the HTTP protocol.
+# REQUEST FILE (upload file)
+install dulu => pip install python-multipart<br>
+kita perlu <b>File</b> param untuk upload<br>
+The files will be uploaded as "form data".
--- main.py
+++ main.py
@@ -1,9 +1,85 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 
-@app.post("/login/")
-# username & password didpt dari input form sehingga bertipe Form()
-async def login(username: str = Form(), password: str = Form()):
-    return {"username": username}
+@app.post("/files/")
+# need File() for upload file
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+# File Parameters with UploadFile
+"""
+kelbhan UploadFile
+* uses a "spooled" file:
+A file stored in memory up to a maximum size limit, and after passing this limit it will be stored in disk.
+* sehingga dpt menyimpan file yg besar tanpa mengonsumsi memory secara penuh
+* atribut UploadFile dpt dilihat https://fastapi.tiangolo.com/tutorial/request-files/#uploadfile
+"""
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+# NILAI DEFAULT PD UPLOADFILE
+@app.post("/default/files/")
+async def create_file(file: bytes | None = File(default=None)):
+    if not file:
+        return {"message": "No file sent"}
+    else:
+        return {"file_size": len(file)}
+
+
+@app.post("/default/uploadfile/")
+async def create_upload_file(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        return {"filename": file.filename}
+
+# ADD METADATA
+@app.post("/addMetadata/files/")
+    # description metadata
+async def create_file(file: bytes = File(description="A file read as bytes")):
+    return {"file_size": len(file)}
+
+
+@app.post("/addMetadata/uploadfile/")
+async def create_upload_file(
+    # description metadata
+    file: UploadFile = File(description="A file read as UploadFile"),
+):
+    return {"filename": file.filename}
+
+# Multiple File Uploads
+# upload several files at the same time. caranya  declare a list of bytes or UploadFile
+@app.post("/multipleupload/files/")
+# list of bytes
+async def create_files(files: list[bytes] = File()):
+# add metadata description
+# async def create_files(files: list[bytes] = File(description="Multiple files as bytes")):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/mulitpleupload/uploadfiles/")
+# list of UploadFile
+async def create_upload_files(files: list[UploadFile]):
+# add metadata description
+# async def create_files(files: list[bytes] = File(description="Multiple files as UploadFile")):
+    return {"filenames": [file.filename for file in files]}
+
+from fastapi.responses import HTMLResponse
+@app.get("/")
+async def main():
+    content = """
+<body>
+    <form action="/mulitpleupload/files/" enctype="multipart/form-data" method="post">
+        <input name="files" type="file" multiple>
+        <input type="submit">
+    </form>
+    <form action="/mulitpleupload/uploadfiles/" enctype="multipart/form-data" method="post">
+        <input name="files" type="file" multiple>
+        <input type="submit">
+    </form>
+</body>
+    """
+    return HTMLResponse(content=content)
