a88ed47d16a2384df5afcc06aa1c15c3a525f2b5
muhsufyan <muh.sufyan.ts@gmail.com>
2022-08-31 20:02:32 +0700

    done

--- README.md
+++ README.md
@@ -13,10 +13,6 @@ di python 3.6, OR pd param func menggunakan union<br>
 sedangkan untuk 3.10<br>
 =>  q: str | None = None
 
-# SUB DEPENDENCIES
-## Using the same dependency multiple times
-jika dependency dideklarasikan beberapa kali didlm path operation, misalnya beberapa dependensi memiliki sub-dependensi yang sama, FastAPI akan tahu untuk memanggil sub-dependensi itu hanya sekali per request.<br>
-Dan itu akan menyimpan nilai yang dikembalikan dalam "cache"<br>
-In an advanced scenario where you know you need the dependency to be called at every step (possibly multiple times) in the same request instead of using the "cached" value, you can set the parameter use_cache=False when using Depends<br>
-async def needy_dependency(fresh_value: str = Depends(get_value, use_cache=False)):
-    return {"fresh_value": fresh_value}
+# Dependencies in path operation decorators
+In some cases you don't really need the return value of a dependency But you still need it to be executed/solved<br>
+so you can add a list of dependencies to the path operation decorator.
--- main.py
+++ main.py
@@ -1,20 +1,42 @@
-from fastapi import Cookie, Depends, FastAPI
+# Add dependencies to the path operation decorator
+
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 app = FastAPI()
 
+# Dependency requirements. declare request requirements (like headers) or other sub-dependencies
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        # dependencies can raise exceptions, the same as normal dependencies
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+        # can return values or not, the values won't be used. FOR THIS NOT RETURN VALUE
 
-def query_extractor(q: str | None = None):
-    return q
+# Dependency requirements. declare request requirements (like headers) or other sub-dependencies
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        # dependencies can raise exceptions, the same as normal dependencies
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    # can return values or not, the values won't be used. FOR THIS RETURN VALUE
+    return x_key
 
-# diinjeksi oleh func query_extractor
-def query_or_cookie_extractor(
-    q: str = Depends(query_extractor), last_query: str | None = Cookie(default=None)
-):
-    if not q:
-        return last_query
-    return q
+# path operation decorator receives an optional argument dependencies. It should be a list of Depends()
+# return value (if they return any) won't be passed to your path operation function but the dependencies will be executed
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
+
+# GLOBAL DEPENDENCY
+# if want add dependencies to the whole application
+
+# THIS GLOBAL DEPENDENCY
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
 
 
-@app.get("/items/")
-async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
-    return {"q_or_cookie": query_or_default}
+@app.get("/withglobaldependencies/items/")
+async def read_items():
+    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
+
+
+@app.get("/withglobaldependencies/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
