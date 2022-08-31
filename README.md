a2fd626647fc3f0481a9e91326b7fcbeed660caa
muhsufyan <muh.sufyan.ts@gmail.com>
2022-08-31 09:45:06 +0700

    done

--- README.md
+++ README.md
@@ -12,12 +12,8 @@ di python 3.6, OR pd param func menggunakan union<br>
 => q: Union[str, None] = None<br>
 sedangkan untuk 3.10<br>
 =>  q: str | None = None
-# Response Model
-adlh data model yg akan diberikan sebagai response ke user<br>
-DIREKOMENDASIKAN RESPONSE DATA MODEL ITU BERBEDA DENGAN DATA MODEL UNTUK DATABASE KARENA USER TDK BUTUH SEMUA DATA YG TER/DI- SIMPAN DLM DATABASE UNTUK DIPERLIHATKAN (AS RESPONSE DATA MODEL)<br>
-EmailString hars install dulu pip install email-validator or pip install pydantic[email].
-
-Use the path operation decorator's parameter response_model to define response models and especially to ensure private data is filtered out.
-
-Use response_model_exclude_unset to return only the values explicitly set.
-## ADA ERROR
+# Extra Models
+kali ini kita akan punya
+* data model input => ada password
+* data model output => tdk ada password
+* data model untuk database => passwordnya dihash
--- main.py
+++ main.py
@@ -1,169 +1,194 @@
-# untuk menggunakan response model gunakan param response_model
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
-
-# data yg diberikan kepada user as response data model adlh data dlm class Item
-@app.post("/inputoutputaresame/items/", response_model=Item)
-async def create_item(item: Item):
-    return item
-
-"""
-FastAPI will use this response_model to:
-
-Convert the output data to its type declaration.
-Validate the data.
-Add a JSON Schema for the response, in the OpenAPI path operation.
-Will be used by the automatic documentation systems.
-"""
-
-# Return the same input data
-from pydantic import EmailStr
-# declaring a UserIn model, it will contain a plaintext password
+# MULITPLE MODELS
 class UserIn(BaseModel):
     username: str
-    # Don't do this in production! because password should not show to user and must encrypted
     password: str
     email: EmailStr
     full_name: str | None = None
-# declare our input and the same model to declare our output. this is very bad, lbh baik data model input dan output (response) itu berbeda & dipisah
-@app.post("/user/", response_model=UserIn)
-async def create_user(user: UserIn):
-    return user
 
-# Add an output model
-# kita pisahkan antara data model input & output (response)
-# dimana password tdk akan ditampilkan (as response data model)
+
 class UserOut(BaseModel):
     username: str
     email: EmailStr
     full_name: str | None = None
 
 
-@app.post("/inputoutputdifferent/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+    email: EmailStr
+    full_name: str | None = None
 
-# Response Model encoding parameters
-# response model could have default values
-class Item2(BaseModel):
-    name: str
-    # defaultnya None
-    description: str | None = None
-    price: float
-    # defaultnya 10.5
-    tax: float = 10.5
-    # # defaultnya list kosong
-    tags: list[str] = []
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+# Pydantic's .dict()
+# pydantic punya model .dict() yg akan mengembalikan suatu data model. contoh
+user_in = UserIn(username="john", password="secret", email="john.doe@example.com")
+# kita ambil dict() dari user_in diatas, merupakan dict bkn objek data model pydantic
+user_dict = user_in.dict()
+print(user_dict)
+print(type(user_dict))
+
+# Unwrapping a dict
+"""
+If we take a dict like user_dict and pass it to a function (or class) with **user_dict, Python will "unwrap" it.
+It will pass the keys and values of the user_dict directly as key-value arguments.
+kode dibawah ini sama saja (cara1==cara2)
+
+"""
+cara1= UserInDB(
+    username="john",
+    hashed_password="secret",
+    email="john.doe@example.com",
+    full_name=None,
+)
+# cara2 = UserInDB(**user_dict) # error karena pada data model UserIn tidak terdpt atribut hashed_password adanya password
+# print(cara2)
+cara3 = UserInDB(
+    username = user_dict["username"],
+    hashed_password = user_dict["password"],
+    email = user_dict["email"],
+    full_name = user_dict["full_name"],
+)
+print(cara1)
+print(cara3)
+
+# # # A Pydantic model from the contents of another
+# # # got user_dict from user_in.dict()
+# user_dict = user_in.dict()
+# # UserInDB(**user_dict)# this will error cause didnt have hashed_password but have password attribut
+# # # kode diatas akan == kode dibawah ini
+# # UserInDB(**user_in.dict())# this will error cause didnt have hashed_password but have password attribut
+# # # because user_in.dict() is a dict, and then we make Python "unwrap" it by passing it to UserInDB prepended with **
+# # # So, we get a Pydantic model from the data in another Pydantic model.
+
+# Unwrapping a dict and extra keywords
+# adding the extra keyword argument hashed_password=hashed_password on user_dict (because user_dict didnt have hashed_password but have password)
+unwrapping1 = UserInDB(**user_in.dict(), hashed_password=fake_password_hasher(user_dict["password"]))
+print(unwrapping1)
+# kode diatas akan sama sprti brkt
+unwrapping2 = UserInDB(
+    username = user_dict["username"],
+    password = user_dict["password"],
+    email = user_dict["email"],
+    full_name = user_dict["full_name"],
+    hashed_password = fake_password_hasher(user_dict["password"]),
+)
+print(unwrapping2)
+
+# Reduce duplication
+# code duplication increments the chances of bugs, security issues, code desynchronization issues (when you update in one place but not in the others), etc.
+# yg duplikasi pd kasus data model adlh atribut username, email & fullname diulangi terus menerus untuk itu kita gunakan konsep inheritance pd data model response
+class UserBase2(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+# inheritance ke UserBase2, ditambah atribut password
+class UserIn2(UserBase2):
+    password: str
+
+# inheritance ke UserBase2
+class UserOut2(UserBase2):
+    pass
+
+# inheritance ke UserBase2,, ditambah atribut hashed_password
+class UserInDB2(UserBase2):
+    hashed_password: str
+
+
+def fake_password_hasher2(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user2(user_in: UserIn2):
+    hashed_password = fake_password_hasher2(user_in.password)
+    user_in_db = UserInDB2(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user2/", response_model=UserOut2)
+async def create_user(user_in: UserIn2):
+    user_saved = fake_save_user2(user_in)
+    return user_saved
+
+# Union or anyOf
+# can declare a response to be the Union of two types, that means, that the response would be any of the two. use anyOf on openapi
+from typing import Union
+class BaseItem(BaseModel):
+    description: str
+    type: str
+
+
+class CarItem(BaseItem):
+    # INI
+    type = "car"
+
+
+class PlaneItem(BaseItem):
+    # INI
+    type = "plane"
+    size: int
 
 
 items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
 }
 
-# You can set the path operation decorator parameter response_model_exclude_unset=True
-"""
-default values won't be included in the response, only the values actually set.
-So, if you send a request to that path operation for the item with ID foo, the response (not including default values) will be:
-{
-    "name": "Foo",
-    "price": 50.2
-}
-selain itu bisa juga memakai 
-response_model_exclude_defaults=True
-response_model_exclude_none=True
-"""
-@app.get("/responsemodel/defaultvalue/items/{item_id}", response_model=Item2, response_model_exclude_unset=True)
-async def read_item(item_id: str):
+# INI OPENAPI. we pass Union[PlaneItem, CarItem] as the value of the argument response_model
+# DI PYTHON 3.10 KITA TETAP MEMAKAI UNION KARENA JIKA response_model=PlaneItem | CarItem AKAN MENYEBABKAN ERROR
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: int):
     return items[item_id]
 
-# response_model_include and response_model_exclude
-# ke2 nya disbt as path operation decorator
-"""
-They take a set of str with the name of the attributes to include (omitting the rest) or to exclude (including the rest).
-This can be used as a quick shortcut if you have only one Pydantic model and want to remove some data from the output.
 
-NOTE
-still recommended to use the ideas above, using multiple classes, instead of these parameters.
-This is because the JSON Schema generated in your app's OpenAPI (and the docs) will still be the one for the complete model, even if you use response_model_include or response_model_exclude to omit some attributes.
-This also applies to response_model_by_alias that works similarly.
-"""
-class Item3(BaseModel):
+# List of models
+# can declare responses of lists of objects
+# use the standard Python typing.List (or just list in Python 3.9 and above)
+class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
-    tax: float = 10.5
+    description: str
 
 
-itemss = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
-    "baz": {
-        "name": "Baz",
-        "description": "There goes my baz",
-        "price": 50.2,
-        "tax": 10.5,
-    },
-}
+items = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
 
 
-@app.get(
-    "/items/{item_id}/name",
-    response_model=Item3,
-    # BAGIAN INI
-    # The syntax {"name", "description"} creates a set with those two values. It is equivalent to set(["name", "description"]).
-    response_model_include={"name", "description"},
-)
-async def read_item_name(item_id: str):
-    return itemss[item_id]
+@app.get("/items/", response_model=list[Item])
+async def read_items():
+    return items
 
-# BAGIAN INI
-@app.get("/items/{item_id}/public", response_model=Item3, response_model_exclude={"tax"})
-async def read_item_public_data(item_id: str):
-    return itemss[item_id]
-
-# Using lists instead of set
-# If you forget to use a set and use a list or tuple instead, FastAPI will still convert it to a set and it will work correctly
-class Item4(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float = 10.5
-
-
-items3 = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
-    "baz": {
-        "name": "Baz",
-        "description": "There goes my baz",
-        "price": 50.2,
-        "tax": 10.5,
-    },
-}
-
-
-@app.get(
-    "/items2/{item_id}/name",
-    response_model=Item4,
-    # INI, sblmnya {"name", "description"}
-    response_model_include=["name", "description"],
-)
-async def read_item_name(item_id: str):
-    return items3[item_id]
-
-# INI, sblmnya {"tax"}
-@app.get("/items2/{item_id}/public", response_model=Item4, response_model_exclude=["tax"])
-async def read_item_public_data(item_id: str):
-    return items3[item_id]
+# Response with arbitrary dict
+# can also declare a response using a plain arbitrary dict, declaring just the type of the keys and values, without using a Pydantic model
+# useful if you don't know the valid field/attribute names (that would be needed for a Pydantic model) beforehand.
+# can use typing.Dict (or just dict in Python 3.9 and above)
+@app.get("/keyword-weights/", response_model=dict[str, float])
+async def read_keyword_weights():
+    return {"foo": 2.3, "bar": 3.4}
