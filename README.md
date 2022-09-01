a6be2b4712fe03d9c9e911a65ab4235c6196a76a
muhsufyan <muh.sufyan.ts@gmail.com>
2022-09-01 10:00:26 +0700

    done

--- README.md
+++ README.md
@@ -13,5 +13,14 @@ di python 3.6, OR pd param func menggunakan union<br>
 sedangkan untuk 3.10<br>
 =>  q: str | None = None
 
-# Get Current User
+# Simple OAuth2 with Password and Bearer
+## passwordnya secret, username dpt dilihat pd fake_users_db
 saya tidak tahu bagaimana cara menggunakan untuk test OAuth2
+penjelasan lbh lanjut mengenai get username & password for OAuth2 https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/#get-the-username-and-password<br>
+body untuk OAuth2PasswordRequestForm adlh :
+* The username.
+* The password.
+* An optional scope field as a big string, composed of strings separated by spaces.
+* An optional grant_type
+* An optional client_id (we don't need it for our example).
+* An optional client_secret (we don't need it for our example).
--- main.py
+++ main.py
@@ -1,31 +1,104 @@
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, FastAPI, HTTPException, status
+# get the username and password with OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
 
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# Create a user model (data model)
+
 class User(BaseModel):
     username: str
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
 
-# Get the user
-def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
 
-# Create a get_current_user dependency. get_current_user will receive a token as a str from the sub-dependency oauth2_scheme
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    # Get the user
-    user = fake_decode_token(token)
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
     return user
 
+# get the current_user only if this user is active
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        # HTTP error if the user doesn't exist
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+# update dependencies. get the current_user only if this user is active
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    # HTTP error if the user doesn't exist
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+# # get the username and password with OAuth2PasswordRequestForm
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+# get the user data from the (fake) database, using the username from the form field.
+# If there is no such user, we return an error saying "incorrect username or password".
+# For the error, we use the exception HTTPException
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+# About **user_dict
+# UserInDB(**user_dict) == 
+# UserInDB(
+#     username = user_dict["username"],
+#     email = user_dict["email"],
+#     full_name = user_dict["full_name"],
+#     disabled = user_dict["disabled"],
+#     hashed_password = user_dict["hashed_password"],
+# )
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    # return token
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
 @app.get("/users/me")
-# Inject the current user(get_current_user)
-async def read_users_me(current_user: User = Depends(get_current_user)):
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
