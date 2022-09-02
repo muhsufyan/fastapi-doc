86bf4955bc8fdfeee30918f9ce8de4ba18f0b095
muhsufyan <muh.sufyan.ts@gmail.com>
2022-09-02 18:07:56 +0700

    done but we not add anything in static folder

--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 python -m venv virtualenv<br>
 windows => .\virtualenv\Scripts\activate.bat<br>
 update => virtualenv\Scripts\python.exe -m pip install --upgrade pip<br>
-run Metadata for API => uvicorn main:app --reload<br>
-run Metadata for tags => uvicorn main2:app --reload<br>
-run OpenAPI URL => uvicorn main3:app --reload<br>
-* dimana app adlh root dir, main adlah file main.py & app adlah FastAPI()<br>
+run => uvicorn main:app --reload<br>
+* dimana main adlah file main.py & app adlah FastAPI()<br>
 # note
 di python 3.6, OR pd param func menggunakan union<br>
 => q: Union[str, None] = None<br>
 sedangkan untuk 3.10<br>
 =>  q: str | None = None
 
-# Metadata & Docs URL
-BACA TABEL DISINI https://fastapi.tiangolo.com/tutorial/metadata/#metadata-for-api<br>
+# Static Files
+ex asset logo, logo perusahaan/startup, dll<br>
+serve static files automatically from a directory using <b>StaticFiles</b><br>
+buat folder namanya static, karena directory isinya static => StaticFiles(directory="static/"), name="static")<br>
--- main.py
+++ main.py
@@ -1,84 +1,17 @@
 from fastapi import FastAPI
+# Use StaticFiles
+from fastapi.staticfiles import StaticFiles
 
-# Metadata for API
-description = """
-ChimichangApp API helps you do awesome stuff. 🚀
+app = FastAPI()
 
-## Items
-
-You can **read items**.
-
-## Users
-
-You will be able to:
-
-* **Create users** (_not implemented_).
-* **Read users** (_not implemented_).
+# "Mount" a StaticFiles() instance in a specific path
+# arti dr mount disini adlh adding a complete "independent" application in a specific path, that then takes care of handling all the sub-paths
 """
+The first "/static" refers to the sub-path this "sub-application" will be "mounted" on
+So, any path that starts with "/static" will be handled by it.
 
-app = FastAPI(
-    # Metadata for API
-    title="ChimichangApp",
-    description=description,
-    version="0.0.1",
-    terms_of_service="http://example.com/terms/",
-    contact={
-        "name": "Deadpoolio the Amazing",
-        "url": "http://x-force.example.com/contact/",
-        "email": "dp@x-force.example.com",
-    },
-    license_info={
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    },
-)
-
-
-@app.get("/metadata/API/items/")
-async def read_items():
-    return [{"name": "Katana"}]
-
-# Metadata for tags
-
-# Create metadata for tags. tags for users and items
-tags_metadata = [
-    {
-        "name": "users",
-        "description": "Operations with users. The **login** logic is also here.",
-    },
-    {
-        "name": "items",
-        "description": "Manage items. So _fancy_ they have their own docs.",
-        "externalDocs": {
-            "description": "Items external docs",
-            "url": "https://fastapi.tiangolo.com/",
-        },
-    },
-]
-# add additional metadata for the different tags used to group your path operations with the parameter openapi_tags
-app = FastAPI(openapi_tags=tags_metadata)
-
-# Use the tags parameter with your path operations (and APIRouters) to assign them to different tags
-@app.get("/metadata/tags/users/", tags=["users"])
-async def get_users():
-    return [{"name": "Harry"}, {"name": "Ron"}]
-
-# Use the tags parameter with your path operations (and APIRouters) to assign them to different tags
-@app.get("/metadata/tags/items/", tags=["items"])
-async def get_items():
-    return [{"name": "wand"}, {"name": "flying broom"}]
-
-
-# OpenAPI URL
+The directory="static" refers to the name of the directory that contains your static files.
+The name="static" gives it a name that can be used internally by FastAPI.
 """
-By default, the OpenAPI schema is served at /openapi.json.
-But you can configure it with the parameter openapi_url.
-For example, to set it to be served at /api/v1/openapi.json
-"""
-
-app = FastAPI(openapi_url="/api/v1/openapi.json")
-
-
-@app.get("/items/")
-async def read_items():
-    return [{"name": "Foo"}]
+# kita harus buat folder static
+app.mount("/static", StaticFiles(directory="static/"), name="static")
